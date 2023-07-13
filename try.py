469940faf9019ae1417a5084c6522f094f3172bf
Ziqi MA <48517009+GuoyuloveSunshine@users.noreply.github.com>
2023-07-13 10:31:27 +0800

    add all

---
+++ try.py
@@ -0,0 +1,13 @@
+import gym, yumi_gym
+
+env = gym.make('yumi-v0')
+observation = env.reset()
+env.render()
+
+
+while True:
+    env.render()
+
+    action = env.action_space.sample()
+
+    observation, reward, done, info = env.step(action)
